bbcfcb2c95c95c25971c1132951f872780ce62c2
K-ost <golubkovru@gmail.com>
2024-11-15 14:35:45 +0200

    cat icon

--- components/Header/NavDrawer.tsx
+++ components/Header/NavDrawer.tsx
@@ -7,6 +7,8 @@ import {
 } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { NAV_ITEMS } from "./constants";
+import catIcon from "@/assets/catalog.svg";
+import Image from "next/image";
 
 type NavDrawerProps = {
   close: () => void;
@@ -28,6 +30,15 @@ const NavDrawer = (props: NavDrawerProps): JSX.Element => {
         {NAV_ITEMS.map((item) => (
           <ListItem key={item.id} disablePadding>
             <ListItemButton onClick={() => clickHandler(item.path)}>
+              {item.path === "/cat" && (
+                <Image
+                  src={catIcon.src}
+                  alt=""
+                  width={28}
+                  height={28}
+                  style={{ marginRight: 12 }}
+                />
+              )}
               <ListItemText primary={item.title} />
             </ListItemButton>
           </ListItem>
