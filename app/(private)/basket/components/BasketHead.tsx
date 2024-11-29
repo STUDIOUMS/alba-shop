60a1bdb78e472eeb6b57ef124daa3162a36ae68f
K-ost <golubkovru@gmail.com>
2024-11-29 23:37:32 +0200

    cart row table - first try

--- app/(private)/basket/components/BasketHead.tsx
+++ app/(private)/basket/components/BasketHead.tsx
@@ -1,6 +1,17 @@
-import { TableCell, TableHead, TableRow } from "@mui/material";
+import {
+  TableCell,
+  TableHead,
+  TableRow,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 const BasketHead = (): JSX.Element => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  if (isMobile) return <></>;
+
   return (
     <TableHead>
       <TableRow>
--- app/(private)/basket/components/BasketRow.tsx
+++ app/(private)/basket/components/BasketRow.tsx
@@ -4,7 +4,15 @@ import PriceBox from "@/components/PriceBox";
 import { useOrderStore } from "@/store/useOrderStore";
 import { Order } from "@/types";
 import { QuantityInput } from "@/ui/QuantityInput";
-import { IconButton, TableCell, TableRow, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  TableCell,
+  TableRow,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Link from "next/link";
 
 type BasketRowProps = {
@@ -15,13 +23,31 @@ const BasketRow = (props: BasketRowProps): JSX.Element => {
   const { order } = props;
   const { deleteOrder, changeCount } = useOrderStore();
   const total = order.count * order.price;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <TableRow sx={{ position: "relative" }}>
       <TableCell component="th" scope="row">
         <Link href={`/product/${order.slug}`}>
-          <img src={order.img} alt="" style={{ display: "block", width: 70 }} />
+          <img
+            src={order.img}
+            alt=""
+            style={{ display: "block", width: isMobile ? 50 : 70 }}
+          />
         </Link>
+        <IconButton
+          color="error"
+          sx={{
+            position: "absolute",
+            right: 0,
+            top: isMobile ? 4 : "50%",
+            mt: isMobile ? 0 : "-24px",
+          }}
+          onClick={() => deleteOrder(order.id)}
+        >
+          &times;
+        </IconButton>
       </TableCell>
       <TableCell>
         <Typography variant="h4">
@@ -30,29 +56,38 @@ const BasketRow = (props: BasketRowProps): JSX.Element => {
         <Typography variant="body2">
           Фасовка: <b>{order.pack}</b>
         </Typography>
-        <Typography variant="body2">
+        <Typography variant="body2" sx={{ mb: isMobile ? 4 : 0 }}>
           Код товара: <b>{order.art}</b>
         </Typography>
+        {isMobile && (
+          <>
+            <PriceBox price={order.price} />
+            <Box display="flex" sx={{ mt: 4, mb: 4 }}>
+              <QuantityInput
+                onChange={(e, value) => changeCount(order.id, String(value))}
+                defaultValue={order.count}
+              />
+            </Box>
+            <PriceBox price={total} />
+          </>
+        )}
       </TableCell>
-      <TableCell>
-        <PriceBox price={order.price} />
-      </TableCell>
-      <TableCell>
-        <QuantityInput
-          onChange={(e, value) => changeCount(order.id, String(value))}
-          defaultValue={order.count}
-        />
-      </TableCell>
-      <TableCell>
-        <PriceBox price={total} />
-        <IconButton
-          color="error"
-          sx={{ position: "absolute", right: 0, top: "50%", mt: "-24px" }}
-          onClick={() => deleteOrder(order.id)}
-        >
-          &times;
-        </IconButton>
-      </TableCell>
+      {!isMobile && (
+        <>
+          <TableCell>
+            <PriceBox price={order.price} />
+          </TableCell>
+          <TableCell>
+            <QuantityInput
+              onChange={(e, value) => changeCount(order.id, String(value))}
+              defaultValue={order.count}
+            />
+          </TableCell>
+          <TableCell>
+            <PriceBox price={total} />
+          </TableCell>
+        </>
+      )}
     </TableRow>
   );
 };
