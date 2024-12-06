4a685833802c73e935e885312df9ee640952a98b
K-ost <golubkovru@gmail.com>
2024-12-06 17:34:50 +0200

    product card tests

---
+++ components/Good/__tests__/Factory.tsx
@@ -0,0 +1,27 @@
+import * as Factory from "factory.ts";
+import { Product, RelatedPack } from "@/types";
+
+export const packFactory = Factory.Sync.makeFactory<RelatedPack>({
+  id: Factory.each((i) => i + 1),
+  img: "http://img.com",
+  oldPrice: "",
+  pack: Factory.each((i) => ({ id: i + 1, name: `${i + 1} шт` })),
+  price: Factory.each((i) => `${i + 1}00`),
+  product: 1,
+});
+
+export const productFactory = Factory.Sync.makeFactory<Product>({
+  id: Factory.each((i) => i),
+  art: "12345",
+  categories: [{ name: "cat", pk: 1, slug: "cat" }],
+  createdAt: new Date("2024-11-03T15:08:43.267311Z"),
+  docs: [],
+  description: "",
+  hit: false,
+  new: false,
+  slug: "product",
+  title: "product",
+  relatedPacks: [],
+  defaultPack: [][0],
+  relatedAttrs: [],
+});
---
+++ components/Good/__tests__/Good.test.tsx
@@ -0,0 +1,56 @@
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Good from "..";
+import { packFactory, productFactory } from "./Factory";
+
+const testPacks = packFactory.buildList(2);
+const testPackSale = packFactory.build({ oldPrice: "300" });
+const testProduct = productFactory.build();
+
+describe("Catalog product card", () => {
+  it("Product with packs", () => {
+    render(
+      <Good
+        el={productFactory.build({
+          defaultPack: testPacks[0],
+          relatedPacks: testPacks,
+        })}
+      />
+    );
+    expect(screen.getByText("100")).toBeDefined();
+    expect(screen.getByText("1 шт")).toBeDefined();
+    expect(screen.getByText("2 шт")).toBeDefined();
+    expect(screen.getByRole("button", { name: "В корзину" })).toBeDefined();
+  });
+
+  it("Product without packs", () => {
+    render(<Good el={testProduct} />);
+    expect(
+      screen.getByRole("button", { name: "Сообщить о наличии" })
+    ).toBeDefined();
+  });
+
+  it("Product with sale", () => {
+    render(
+      <Good
+        el={productFactory.build({
+          relatedPacks: [testPackSale],
+          defaultPack: testPackSale,
+        })}
+      />
+    );
+    screen.debug();
+    expect(screen.getByText("sale")).toBeDefined();
+  });
+
+  it("Product with hit", () => {
+    render(<Good el={productFactory.build({ hit: true })} />);
+    expect(screen.getByText("hit")).toBeDefined();
+  });
+
+  it("Product with new", () => {
+    render(<Good el={productFactory.build({ new: true })} />);
+    expect(screen.getByText("new")).toBeDefined();
+  });
+});
