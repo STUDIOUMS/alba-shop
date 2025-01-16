44d2521f26bb59e1e1b350c60f1ef3e66a887c92
K-ost <golubkovru@gmail.com>
2025-01-16 13:13:12 +0200

    changing fail page

--- app/(private)/fail/page.tsx
+++ app/(private)/fail/page.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Section from "@/ui/Section";
 import { Alert, Typography } from "@mui/material";
-import { TEXTS } from "@/texts";
 import { PaymentStatus } from "../types";
 import BreadCrumbs from "@/ui/BreadCrumbs";
 import { paymentCrumbs } from "../constants";
@@ -26,18 +25,15 @@ const FailedPayment = (): JSX.Element => {
     }
   }, [payId, mutate]);
 
-  if (isSuccess && data.status === "ok")
-    return (
-      <Section>
-        <BreadCrumbs links={paymentCrumbs} />
-        <Typography variant="h1">Оплата заказа не совершена</Typography>
-        <Alert variant="outlined" color="error" sx={{ mb: 6 }}>
-          {TEXTS.notifications.failedPayment}
-        </Alert>
-      </Section>
-    );
-
-  return <></>;
+  return (
+    <Section>
+      <BreadCrumbs links={paymentCrumbs} />
+      <Typography variant="h1">Оплата заказа</Typography>
+      <Alert variant="outlined" color="error" sx={{ mb: 6 }}>
+        {isSuccess && data.message}
+      </Alert>
+    </Section>
+  );
 };
 
 export default FailedPayment;
